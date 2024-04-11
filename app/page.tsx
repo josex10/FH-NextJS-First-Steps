f5240b47ac4bfa7edcba9e5f3e224ec6a716df50
jose.badilla <jose.badilla@First-Factory-Inc-MacBookPro-P3997649J9.local>
2024-04-11 17:09:30 -0600

    setting dockerfile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center p-24">
       <span className="text-5xl">Hello World!!!</span>
-      <Link href={'/about'}>About Page</Link>
+      <Link prefetch={false} href={'/about'}>About Page</Link>
     </main>
   );
 }
